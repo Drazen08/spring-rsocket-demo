0d0566882451a28bde850a37157cdad772e4a61a
sunjx <drazen08@163.com>
2020-09-02 14:21:13 +0800

    rsocket client like feign client

--- rsocket-client/src/main/java/io/pivotal/rsocketclient/adapter/RSocketAspect.java
+++ rsocket-client/src/main/java/io/pivotal/rsocketclient/adapter/RSocketAspect.java
@@ -14,11 +14,9 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.time.Duration;
-import java.util.ServiceLoader;
+
 
 /**
  * @author ：sunjx
@@ -28,38 +26,37 @@ import java.util.ServiceLoader;
 @Component
 @Aspect
 @Slf4j
-public class RSocketAspect {
+public class RSocketAspect{
 
     @Autowired
     private RequestCompoent requestCompoent;
 
-
-    //    @Pointcut("@annotation(io.pivotal.rsocketclient.adapter.RSocketMethod)")
+//        @Pointcut("@annotation(io.pivotal.rsocketclient.adapter.RSocketMethod)")
     @Pointcut("execution( * io.pivotal.rsocketclient.client85.*.*(..))")
     public void cut() {
     }
 
     @Around("cut()")
     public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        Signature     signature         = pjp.getSignature();
-        Class         declaringType     = signature.getDeclaringType();
-        String        name              = signature.getName();
-        Object[] args = pjp.getArgs();
+        long      startTime     = System.currentTimeMillis();
+        Signature signature     = pjp.getSignature();
+        Class     declaringType = signature.getDeclaringType();
+        String    name          = signature.getName();
+        Object[]  args          = pjp.getArgs();
 
         // 获取interface
         Class<?>      iRsocketClient = RSocketClientHandler.findIRsocketClient(declaringType);
         RSocketServer anno           = iRsocketClient.getAnnotation(RSocketServer.class);
         String        serviceId      = anno.serviceId();
-        Method lastMethod = MethodFinderUtil.getLastMethod(iRsocketClient, name);
+        Method        lastMethod     = MethodFinderUtil.getLastMethod(iRsocketClient, name);
         if (lastMethod != null) {
             RSocketRequester requester = requestCompoent.create(serviceId);
-            RSocketMethod   mm        = lastMethod.getAnnotation(RSocketMethod.class);
-            String         value     = mm.value();
+            RSocketMethod    mm        = lastMethod.getAnnotation(RSocketMethod.class);
+            String           value     = mm.value();
             if (!StringUtil.isNullOrEmpty(value)) {
 
                 Mono<?> mono = requester.route(value)
-                        .data(args.length > 0?args[0]:null)
+                        .data(args.length > 0 ? args[0] : null)
                         .retrieveMono(lastMethod.getReturnType())
                         .retryBackoff(1, Duration.ofMillis(100));
 
